5d70edf3a3d972b3328cf43f20a3d6987d08fb38
SHA-4 <alexm@mighty.com>
2020-11-02 00:48:08 -0500

    Compare user-inputted text to random entry in pronoun file

--- src/transition.go
+++ src/transition.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+        "bufio"
         "encoding/csv"
         "fmt"
         "log"
+        "math/rand"
         "os"
+        "strings"
+        "time"
 )
 
 func main() {
@@ -15,11 +19,28 @@ func main() {
         defer f.Close()
 
         csvReader := csv.NewReader(f)
+        // pronouns file format is NAME, SUBJECT PRONOUN, OBJECT PRONOUN
+        // e.g. Mark he him
         pronouns, err := csvReader.ReadAll()
         if err != nil {
                 log.Fatal("Error reading csv data", err)
         }
 
-        fmt.Println("Data", pronouns)
+        for {
+                runTest(pronouns)
+        }
 }
 
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+func runTest(pronouns [][]string) {
+        randomPronoun := pronouns[rand.Intn(len(pronouns))]
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Print(randomPronoun[0] + "\n")
+        text, _ := reader.ReadString('\n')
+        formattedText := strings.ToLower(strings.TrimSuffix(text, "\n"))
+
+        fmt.Println(strings.ToLower(randomPronoun[1]) == formattedText, "\n")
+}
